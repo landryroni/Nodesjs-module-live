7a4d3437214a6dda03e83c41f304af34285f8b58
Dejoli Tientcheu Touko Landry <1197581424@qq.com>
2023-04-02 10:43:21 +0000

    Add inde.js to our respository

---
+++ index.js
@@ -0,0 +1,7 @@
+let num1 = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[3]);
+let result = num1 + num2
+console.log(`The sum of numbers ${num1} and ${num2} is ${result}`)
+
+// console.log(process.argv[2])
+// console.log(process.argv[3])
